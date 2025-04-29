fec74e55c20b5a538890d30dd4fd1cd537224217
za5en <za5en@yandex.ru>
2025-04-29 20:21:22 +0400

    appbar upd

--- lib/view/widgets/a_appbar.dart
+++ lib/view/widgets/a_appbar.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class AAppBar extends StatelessWidget implements PreferredSizeWidget {
   const AAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AppBar(
+      centerTitle: true,
+      title: SvgPicture.asset('assets/images/logo.svg'),
+      leading: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Visibility(
+          visible: true, // depending on settings
+          child: InkWell(
+            onTap: () {
+              // open navigation drawer
+            },
+            child: SvgPicture.asset('assets/images/menu-button.svg'),
+          ),
+        ),
+      ),
+      actions: [
+        Visibility(
+          visible: false, // depending on settings
+          child: InkWell(
+            onTap: () {
+              // open navigation drawer
+            },
+            child: SvgPicture.asset('assets/images/menu-button.svg'),
+          ),
+        ),
+      ],
+    );
   }
 
   @override
